ccabf33c28b9a7f6474c7c75b7cad344409c82ab
Mikhail <shchurousky@gmail.com>
2021-05-17 12:56:28 +0300

    sqt settings

--- spa_ns/settings.py
+++ spa_ns/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ad8n9r+1n#puisng3f-_5-uadjolm85$sza019!9*3bdmiafyj'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
